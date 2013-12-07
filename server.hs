54eb3e514d8015a764b3de6e7ff6c518f255e5bf
Matias De Santi <mdesanti90@gmail.com>
2013-12-07 17:17:28 -0300

    Posts can be updated

--- server.hs
+++ server.hs
@@ -10,7 +10,7 @@ import Control.Monad.State
 import Control.Monad.Reader ( ask )
 import Control.Exception    ( bracket )
 import Happstack.Server(Method(GET, HEAD, POST), dir, methodM, ServerPart, Response,
-                        toResponse, simpleHTTP, nullConf, ok, toMessage, look,
+                        toResponse, simpleHTTP, nullConf, ok, toMessage, look, lookRead,
                         defaultBodyPolicy, BodyPolicy, decodeBody, RqData,
                         getDataFn, badRequest, lookFile, path, resp)
 import           Text.Blaze
@@ -80,7 +80,7 @@ addPost post_title post_content =
 getPost :: PostId -> Query Blog (Maybe BlogPost)
 getPost key = 
     do Blog{..} <- ask
-       return $ getOne $ posts @= key
+       return (getOne (posts @= key))
 
 
 allPosts :: Query Blog  [BlogPost]
@@ -89,7 +89,14 @@ allPosts = do
              let all_posts = IxSet.toList posts
              return all_posts
 
-$(makeAcidic ''Blog ['addPost, 'allPosts, 'getPost])
+updatePost :: BlogPost -> Update Blog ()
+updatePost (BlogPost key title content) = do
+  b@Blog{..} <- get
+  put $ b { posts =
+             IxSet.updateIx key (BlogPost key title content) posts
+          }
+
+$(makeAcidic ''Blog ['addPost, 'allPosts, 'getPost, 'updatePost])
 
 ------------------------------------------ ACID CONFIGURATION ------------------------------------------
 ------------------------------------------ MAIN --------------------------------------------------------
@@ -105,42 +112,50 @@ main =
               (\acid -> simpleHTTP nullConf $
                           do decodeBody myPolicy
                              msum [ do dir "upload" $ do methodM [GET, HEAD] 
-                                       uploadForm acid,
+                                       newForm acid,
                                     do dir "create_post" $ do methodM [POST]
-                                       handleForm acid,
+                                       handleNewForm acid,
+                                    do dir "update_post" $ do methodM [POST]
+                                       handleEditForm acid,
                                     do dir "allPosts" $ do methodM [GET]
                                        handleAllPosts acid,
                                     do dir "posts" $ do path $ (\s -> showPost acid s),
+                                    do dir "update_post" $ do path $ (\s -> editForm acid s),
                                     home acid
                                   ])
 ------------------------------------------ MAIN --------------------------------------------------------
 
 ------------------------------------------ POST UPLOAD -------------------------------------------------
 
-postRq :: RqData (String, String)
+postRq :: RqData (String, String, Integer)
 postRq =
-    (,) <$> look "post_title" <*> look "post_content"
+    (,,) <$> look "post_title" <*> look "post_content" <*> lookRead "post_id"
 
-uploadForm :: AcidState Blog -> ServerPart Response
-uploadForm acid = ok $ toResponse $
+newForm :: AcidState Blog -> ServerPart Response
+newForm acid = createForm acid (BlogPost (PostId 0) "" "") "/create_post"
+
+
+createForm :: AcidState Blog -> BlogPost -> String -> ServerPart Response
+createForm acid (BlogPost (PostId key) title content) post_url = ok $ toResponse $
     appTemplate "Programación Funcional" []
       (H.form ! A.enctype "multipart/form-data"
             ! A.method "POST"
-            ! A.action "/create_post" $ do
+            ! A.action (stringValue post_url) $ do
                H.label "Post Title"
-               H.input ! A.type_ "text" ! A.name "post_title"
+               H.input ! A.type_ "text" ! A.name "post_title" ! A.value (stringValue title)
                H.label "Post Content"
-               H.textarea ! A.type_ "text" ! A.name "post_content" ! A.id "name" $ ""
+               H.textarea ! A.type_ "text" ! A.name "post_content" ! A.cols (H.toValue (60 ::Integer)) ! A.rows (H.toValue (10 ::Integer)) $ (H.toHtml content)
+               H.input ! A.type_ "hidden" ! A.name "post_id" ! A.value (stringValue (show key))
                H.input ! A.type_ "submit" ! A.value "upload"
       )
 
 
-handleForm :: AcidState Blog -> ServerPart Response
-handleForm acid =
+handleNewForm :: AcidState Blog -> ServerPart Response
+handleNewForm acid =
    do post_data <- getDataFn postRq
       case post_data of
         Left e -> badRequest (toResponse (unlines e))
-        Right(post_title, post_content) -> 
+        Right(post_title, post_content, post_id) -> 
                     do c <- update' acid (AddPost post_title post_content)
                        ok $ toResponse $ 
                          appTemplate "Programación Funcional" [] (mkBody post_title post_content)      
@@ -149,6 +164,32 @@ handleForm acid =
                                H.p (H.toHtml $ "Post Title: " ++ post_title)
                                H.p (H.toHtml $ "Post Content:  " ++ post_content)
 ------------------------------------------ POST UPLOAD -------------------------------------------------
+
+------------------------------------------ POST UPDATE -------------------------------------------------
+
+
+editForm :: AcidState Blog -> Integer -> ServerPart Response
+editForm acid key = 
+                    do post <- query' acid (GetPost (PostId key))
+                       case post of
+                        Just (BlogPost a b c) -> createForm acid (BlogPost a b c) "/update_post"
+                        Nothing -> badRequest (toResponse (("Could not find post with id " ++ show key) :: String))
+
+
+handleEditForm :: AcidState Blog -> ServerPart Response
+handleEditForm acid =
+   do post_data <- getDataFn postRq
+      case post_data of
+        Left e -> badRequest (toResponse (unlines e))
+        Right(post_title, post_content, post_id) -> 
+                    do c <- update' acid (UpdatePost (BlogPost (PostId post_id) post_title post_content))
+                       ok $ toResponse $ 
+                         appTemplate "Programación Funcional" [] (mkBody post_title post_content)      
+                           where
+                             mkBody post_title post_content = do
+                               H.p (H.toHtml $ "Post Title: " ++ post_title)
+                               H.p (H.toHtml $ "Post Content:  " ++ post_content)
+------------------------------------------ POST UPDATE -------------------------------------------------
 
 -------------------------------------------- TEMPLATE --------------------------------------------------
 appTemplate :: String -> [H.Html] -> H.Html -> H.Html
@@ -170,7 +211,7 @@ showPost acid post_id =
             do post <- query' acid (GetPost (PostId post_id))
                case post of
                   Just post -> buildShowResponse post
-                  Nothing -> badRequest (toResponse ("Hola" :: String))
+                  Nothing -> badRequest (toResponse (("Could not find post with id " ++ show post_id) :: String))
 
 buildShowResponse :: BlogPost -> ServerPart Response
 buildShowResponse (BlogPost _ post_title post_content) = 
